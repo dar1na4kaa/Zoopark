5528563c86b77c63be6ec837aad3f925335d9f99
Darina4ka <132387009+dar1na4kaa@users.noreply.github.com>
2023-10-17 23:10:24 +0300

    Add files via upload

---
+++ view_accommodation.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zoo
+{
+    /// <summary>
+    /// Логика взаимодействия для view_accommodation.xaml
+    /// </summary>
+    public partial class view_accommodation : Page
+    {
+        public view_accommodation()
+        {
+            InitializeComponent();
+        }
+    }
+}
